e8bde880c8e5269d7868f5e8daa11e7789b83d2f
Truong Vi <nltruongvi@gmail.com>
2024-10-08 10:54:05 +0700

    chore: update ecosystem config

---
+++ strapi/ecosystem.config.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+module.exports = {
+  apps: [
+    {
+      name: "hrm-strapi-service",
+      script: "yarn",
+      args: "start",
+      exec_mode: "fork",
+      watch: true,
+    },
+  ],
+};
