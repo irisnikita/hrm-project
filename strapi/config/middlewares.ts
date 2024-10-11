0198179eb4cb9e95315aa208e8bbcb6fcb1be01e
Truong Vi <nltruongvi@gmail.com>
2024-10-11 15:33:41 +0700

    chore(config): update enable origin * fro cors

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -2,11 +2,17 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: '*',
+    }
+  }
 ];
